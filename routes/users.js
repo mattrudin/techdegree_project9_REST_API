367c70e6cf6f5a3397d1845dc7e48adc818e5c46
Matt Rudin <matthias.rudin@live.com>
2019-03-31 12:49:53 +0200

    Add validation checking for POST /api/users

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 /************************************************************************************
 api/users Routes
@@ -10,7 +11,27 @@ router.get('/', (req, res) => {
 });
 
 // POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
-router.post('/', (req, res) => {
+router.post('/', [
+    check('firstName')
+      .exists()
+      .withMessage('Please provide a value for "firstName"'),
+    check('lastName')
+      .exists()
+      .withMessage('Please provide a value for "lastName"'),
+    check('emailAddress')
+        .exists()
+        .withMessage('Please provide a value for "emailAddress'),
+    check('password')
+      .exists()
+      .withMessage('Please provide a value for "password"'),
+  ], (req, res) => {
+    // Error checking  
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map(error => error.msg);
+      return res.status(400).json({ errors: errorMessages });
+    }
+
     const user = req.body;
     // TODO: Add user to the database
     res.status(201).end();
