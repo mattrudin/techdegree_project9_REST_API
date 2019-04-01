b5cc7f978935f9909d11bf3e4199b09b9e8b69fc
Matt Rudin <matthias.rudin@live.com>
2019-04-01 12:18:49 +0200

    Update /api/users routes

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,19 @@ api/users Routes
 ************************************************************************************/
 // GET /api/users 200 - Returns the currently authenticated user
 router.get('/', async (req, res, next) => {
-    const user = await User.find({}).exec((error, user) => error ? next(error) : res.status(200).json(user));
+    const user = await User.find({}).exec((error, user) => {
+        if(error) return next(error);
+        res.status(200).json(user);
+    });
 });
 
 // POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
 router.post('/', async (req, res, next) => {
     const userToBeCreated = req.body;
-    const user = await User.create(userToBeCreated, (error) => error ? next(error) : res.status(201).end());
+    const user = await User.create(userToBeCreated, (error) => {
+        if(error) return next(error);
+        res.status(201).end();
+    });
 })
 
 module.exports = router;
