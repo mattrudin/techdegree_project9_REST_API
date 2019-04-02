da2a1982f75d3a53cd66ca792ab61e9bb0600ce8
Matt Rudin <matthias.rudin@live.com>
2019-04-02 17:39:06 +0200

    Update routes with try/catch blocks

--- routes/courses.js
+++ routes/courses.js
@@ -7,43 +7,57 @@ api/courses Routes
 ************************************************************************************/
 // GET /api/courses 200 - Returns a list of courses (including the user that owns each course)
 router.get('/', async (req, res, next) => {
-    const course = await Course.find({}).exec((error, course) => {
-        if(error) return next(error);
+    try {
+        const course = await Course.find({})
+            .lean()
+            .exec();
         res.status(200).json(course);
-    });
+    } catch (error) {
+        next(error);
+    }
 });
 // GET /api/courses/:id 200 - Returns a course (including the user that owns the course) for the provided course ID
 router.get('/:id', async (req, res, next) => {
-    const id = req.params.id
-    const course = await Course.findById({_id: id}).exec((error, course) => {
-        if(error) return next(error);
-        res.status(200).json(course)
-    });
+    const courseId = req.params.id
+    try {
+        const course = await Course.findById(courseId)
+            .lean()
+            .exec();
+        res.status(200).json(course);
+    } catch (error) {
+        next(error);
+    }
 });
 // POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
 router.post('/', async (req, res, next) => {
     const courseToBeCreated = req.body;
-    const course = await Course.create(courseToBeCreated, (error) => {
-        if(error) return next(error);
+    try {
+        const course = await Course.create(courseToBeCreated);
         res.status(201).end();
-    });
+    } catch (error) {
+        next(error);
+    }
 })
 // PUT /api/courses/:id 204 - Updates a course and returns no content
 router.put('/:id', async (req, res, next) => {
-    const id = req.params.id;
+    const courseId = req.params.id;
     const courseToBeUpdated = req.body;
-    const course = await Course.findByIdAndUpdate(id, req.body, (error) => {
-        if(error) return next(error);
+    try {
+        const course = await Course.findByIdAndUpdate(courseId, courseToBeUpdated);
         res.status(204).end();
-    });
+    } catch (error) {
+        next(error);
+    }
 })
 // DELETE /api/courses/:id 204 - Deletes a course and returns no content
 router.delete('/:id', async (req, res, next) => {
-    const id = req.params.id;
-    const course = await Course.findByIdAndDelete(id, (error) => {
-        if(error) return next(error);
+    const courseId = req.params.id;
+    try {
+        const course = await Course.findByIdAndDelete(courseId);
         res.status(204).end();
-    })
+    } catch (error) {
+        next(error);
+    }
 })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,19 +7,24 @@ api/users Routes
 ************************************************************************************/
 // GET /api/users 200 - Returns the currently authenticated user
 router.get('/', async (req, res, next) => {
-    const user = await User.find({}).exec((error, user) => {
-        if(error) return next(error);
-        res.status(200).json(user);
-    });
+    try {
+        const user = await User.find({})
+            .exec();
+            res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
 });
 
 // POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
 router.post('/', async (req, res, next) => {
     const userToBeCreated = req.body;
-    const user = await User.create(userToBeCreated, (error) => {
-        if(error) return next(error);
+    try {
+        const user = await User.create(userToBeCreated);
         res.status(201).end();
-    });
+    } catch (error) {
+        next(error);
+    }
 })
 
 module.exports = router;
