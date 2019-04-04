cc46e6a7f9584d51a91d9ca42b62de83166450f5
Matt Rudin <matthias.rudin@live.com>
2019-04-04 17:30:24 +0200

    Organized routes

--- routes/courses.js
+++ routes/courses.js
@@ -4,102 +4,109 @@ const authenticateUser = require('../utility/auth');
 const { Course } = require('../models/models');
 
 /************************************************************************************
-api/courses Routes
+api/courses/ Routes
 ************************************************************************************/
-// GET /api/courses 200 - Returns a list of courses (including the user that owns each course)
-router.get('/', async (req, res, next) => {
-    try {
-        // Get all courses from the database
-        const course = await Course.find({})
-            .lean()
-            // Return only the firstName and lastName of the given user
-            .populate("user", "firstName lastName")
-            .exec();
-        // Return all available courses
-        res.status(200).json(course);
-    } catch (error) {
-        // Throw error
-        next(error);
-    }
-});
-// GET /api/courses/:id 200 - Returns a course (including the user that owns the course) for the provided course ID
-router.get('/:id', async (req, res, next) => {
-    const courseId = req.params.id
-    try {
-        // Get the specified courses from the database
-        const course = await Course.findById(courseId)
-            .lean()
-            // Return only the firstName and lastName of the given user
-            .populate("user", "firstName lastName")
-            .exec();
-        // Return the course
-        res.status(200).json(course);
-    } catch (error) {
-        // Throw error
-        next(error);
-    }
-});
-// POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
-router.post('/', authenticateUser, async (req, res, next) => {
-    // Get the course title, description (required) and the optional assets
-    const courseToBeCreated = req.body;
-    // Get the current logged in user id
-    const loggedInUserID = req.currentUser._id;
-    // Set the coure user to the current logged in user
-    courseToBeCreated.user = loggedInUserID;
-
-    try {
-        // Create the course in the database
-        const course = await Course.create(courseToBeCreated);
-        const courseID = course.id;
-        // Set the response header to '/:id'
-        res.location(`/${courseID}`);
-        // Return nothing
-        res.status(201).end();
-    } catch (error) {
-        // Throw validation error
-        if(error.name === "ValidationError") {
-            return res.status(400).json({ error: error.message});
+router.route('/')
+    // GET /api/courses 200 - Returns a list of courses (including the user that owns each course)
+    .get(async (req, res, next) => {
+        try {
+            // Get all courses from the database
+            const course = await Course.find({})
+                .lean()
+                // Return only the firstName and lastName of the given user
+                .populate("user", "firstName lastName")
+                .exec();
+            // Return all available courses
+            res.status(200).json(course);
+        } catch (error) {
+            // Throw error
+            next(error);
         }
-        // Throw error
-        next(error);
-    }
-})
-// PUT /api/courses/:id 204 - Updates a course and returns no content
-router.put('/:id', authenticateUser, async (req, res, next) => {
-    const courseId = req.params.id;
-    const courseToBeUpdated = req.body;
-    // Mongoose update option: Required!
-    const opts = { runValidators: true };
-    try {
-        // Update the course in the database
-        const course = await Course.findByIdAndUpdate(courseId, courseToBeUpdated, opts);
-        // Return nothing
-        res.status(204).end();
-    } catch (error) {
-        // Throw validation error
-        // Will only throw an error if title or description is "" (empty)
-        if(error.name === "ValidationError") {
-            return res.status(400).json({ error: error.message});
+    })
+    // POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
+    .post(authenticateUser, async (req, res, next) => {
+        // Get the course title, description (required) and the optional assets
+        const courseToBeCreated = req.body;
+        // Get the current logged in user id
+        const loggedInUserID = req.currentUser._id;
+        // Set the coure user to the current logged in user
+        courseToBeCreated.user = loggedInUserID;
+    
+        try {
+            // Create the course in the database
+            const course = await Course.create(courseToBeCreated);
+            const courseID = course.id;
+            // Set the response header to '/:id'
+            res.location(`/${courseID}`);
+            // Return nothing
+            res.status(201).end();
+        } catch (error) {
+            // Throw validation error
+            if(error.name === "ValidationError") {
+                return res.status(400).json({ error: error.message});
+            }
+            // Throw error
+            next(error);
         }
-        // Throw error
-        next(error);
-    }
-})
-// DELETE /api/courses/:id 204 - Deletes a course and returns no content
-router.delete('/:id', authenticateUser, async (req, res, next) => {
-    const courseId = req.params.id;
-    try {
-        // Delete the course in the database
-        const course = await Course.findByIdAndDelete(courseId);
-        // Return nothing
-        res.status(204).end();
-    } catch (error) {
-        // Throw error
-        next(error);
-    }
-})
+    })
 
+
+/************************************************************************************
+api/courses/:id Routes
+************************************************************************************/
+router.route('/:id')
+    // GET /api/courses/:id 200 - Returns a course (including the user that owns the course) for the provided course ID
+    .get(async (req, res, next) => {
+        const courseId = req.params.id
+        try {
+            // Get the specified courses from the database
+            const course = await Course.findById(courseId)
+                .lean()
+                // Return only the firstName and lastName of the given user
+                .populate("user", "firstName lastName")
+                .exec();
+            // Return the course
+            res.status(200).json(course);
+        } catch (error) {
+            // Throw error
+            next(error);
+        }
+    })
+    // PUT /api/courses/:id 204 - Updates a course and returns no content
+    .put(authenticateUser, async (req, res, next) => {
+        const courseId = req.params.id;
+        const courseToBeUpdated = req.body;
+        // Mongoose update option: Required!
+        const opts = { runValidators: true };
+        try {
+            // Update the course in the database
+            const course = await Course.findByIdAndUpdate(courseId, courseToBeUpdated, opts);
+            // Return nothing
+            res.status(204).end();
+        } catch (error) {
+            // Throw validation error
+            // Will only throw an error if title or description is "" (empty)
+            if(error.name === "ValidationError") {
+                return res.status(400).json({ error: error.message});
+            }
+            // Throw error
+            next(error);
+        }
+    })
+    // DELETE /api/courses/:id 204 - Deletes a course and returns no content
+    .delete(authenticateUser, async (req, res, next) => {
+        const courseId = req.params.id;
+        try {
+            // Delete the course in the database
+            const course = await Course.findByIdAndDelete(courseId);
+            // Return nothing
+            res.status(204).end();
+        } catch (error) {
+            // Throw error
+            next(error);
+        }
+    })
+    
 
 /************************************************************************************
 Export route
--- routes/users.js
+++ routes/users.js
@@ -7,53 +7,53 @@ const authenticateUser = require('../utility/auth');
 /************************************************************************************
 api/users Routes
 ************************************************************************************/
-// GET /api/users 200 - Returns the currently authenticated user
-router.get('/', authenticateUser, async (req, res, next) => {
-    // Get the user information from the request
-    const loggedInUser = req.currentUser;
-    const name = loggedInUser.emailAddress;
-    try {
-        // Check if the user is available in the database
-        const user = await User.findOne({"emailAddress": name})
-            .lean()
-            .exec();
-            // Return the authenticated user
-            res.status(200).json(user);
-    } catch (error) {
-        // Throw error
-        next(error);
-    }
-});
-
-// POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
-router.post('/', async (req, res, next) => {
-    try {
-        const userToBeCreated = req.body;
-        const userPassword = userToBeCreated.password;
-        // Check if the user provided a password
-        if(userPassword) {
-            // Hash the user password
-            userToBeCreated.password = bcryptjs.hashSync(userPassword);
-            // Create the user in the database
-            const user = await User.create(userToBeCreated);
-            // Set the response header to '/'
-            res.location('/');
-            // Return nothing
-            res.status(201).end();
-        } else {
-            const error = new Error("User validation failed: password is required.");
-            error.name = "ValidationError";
-            throw error;
+router.route('/')
+    // GET /api/users 200 - Returns the currently authenticated user
+    .get(authenticateUser, async (req, res, next) => {
+        // Get the user information from the request
+        const loggedInUser = req.currentUser;
+        const name = loggedInUser.emailAddress;
+        try {
+            // Check if the user is available in the database
+            const user = await User.findOne({"emailAddress": name})
+                .lean()
+                .exec();
+                // Return the authenticated user
+                res.status(200).json(user);
+        } catch (error) {
+            // Throw error
+            next(error);
         }
-    } catch (error) {
-        // Throw validation error
-        if(error.name === "ValidationError") {
-            return res.status(400).json({ error: error.message});
+    })
+    // POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
+    .post(async (req, res, next) => {
+        try {
+            const userToBeCreated = req.body;
+            const userPassword = userToBeCreated.password;
+            // Check if the user provided a password
+            if(userPassword) {
+                // Hash the user password
+                userToBeCreated.password = bcryptjs.hashSync(userPassword);
+                // Create the user in the database
+                const user = await User.create(userToBeCreated);
+                // Set the response header to '/'
+                res.location('/');
+                // Return nothing
+                res.status(201).end();
+            } else {
+                const error = new Error("User validation failed: password is required.");
+                error.name = "ValidationError";
+                throw error;
+            }
+        } catch (error) {
+            // Throw validation error
+            if(error.name === "ValidationError") {
+                return res.status(400).json({ error: error.message});
+            }
+            // Throw error
+            next(error);
         }
-        // Throw error
-        next(error);
-    }
-})
+    })
 
 
 /************************************************************************************
